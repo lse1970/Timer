89ae9e2f93f5e76b2d6a20d8a931de287d4ad13a
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-10 15:47:47 +0900

    timer

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var swt: UISwitch!
+    var myTimer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+        swt.setOn(false, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EE"
+        lbl.text = formatter.string(from: date)
+        
     }
-
-
+    @IBAction func sw(_ sender: Any) {
+        if swt.isOn == true  {
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        } else  {
+            myTimer.invalidate()
+        }
+    }
 }
-
